c2f407834df86d6354a985e7ec0f256d42f4cee0
Gae24 <96017547+Gae24@users.noreply.github.com>
2023-04-10 11:33:58 +0200

    fix layout per nuova stanza fragment

--- Client/app/src/main/java/com/example/chat2023/ui/nuovaStanza/NuovaStanzaFragment.java
+++ Client/app/src/main/java/com/example/chat2023/ui/nuovaStanza/NuovaStanzaFragment.java
@@ -9,26 +9,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.chat2023.R;
 import com.example.chat2023.databinding.FragmentNuovaStanzaBinding;
 
 public class NuovaStanzaFragment extends Fragment {
-
-    private FragmentNuovaStanzaBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         NuovaStanzaViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(NuovaStanzaViewModel.class);
 
-        binding = FragmentNuovaStanzaBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        return root;
+        View view = inflater.inflate(R.layout.fragment_nuova_stanza, container, false);
+        return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
