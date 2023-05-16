dc1866e88c0d255bbd655d61d1c561f7ce9fb771
Gae24 <96017547+Gae24@users.noreply.github.com>
2023-05-16 12:11:24 +0200

    aggiunto menu in ChatActivity

--- Client/app/src/main/java/com/mikefonseta/chatx/Activity/ChatActivity.java
+++ Client/app/src/main/java/com/mikefonseta/chatx/Activity/ChatActivity.java
@@ -2,13 +2,19 @@ package com.mikefonseta.chatx.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputLayout;
 import com.mikefonseta.chatx.Adapter.MessageListAdapter;
 import com.mikefonseta.chatx.Controller.ChatController;
 import com.mikefonseta.chatx.Controller.Controller;
@@ -20,6 +26,8 @@ public class ChatActivity extends AppCompatActivity {
     private MessageListAdapter messageListAdapter;
     private EditText messageContent;
     private RecyclerView messageRecycler;
+    private View dialogView;
+    private TextInputLayout textInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,9 @@ public class ChatActivity extends AppCompatActivity {
         Controller.setCurrentActivity(this);
         ConnectionHandler.getInstance().doRequest(ChatController.getMessageRequest(room_id));
 
+        dialogView = getLayoutInflater().inflate(R.layout.edit_room, null);
+        textInputLayout = dialogView.findViewById(R.id.room_input_layout);
+
         messageRecycler = findViewById(R.id.message_recycler_view);
         messageRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         messageListAdapter = new MessageListAdapter();
@@ -43,6 +54,21 @@ public class ChatActivity extends AppCompatActivity {
             if (!messageContent.getText().toString().equals("") && messageContent.getText().toString().length() > 0) {
                 ConnectionHandler.getInstance().doRequest(ChatController.getSendMessageRequest(messageContent.getText().toString(), room_id));
             }
+        });
+
+        ImageButton menuButton = findViewById(R.id.menu);
+        menuButton.setOnClickListener(view -> {
+            PopupMenu menu = new PopupMenu(this, view);
+            menu.inflate(R.menu.owner_menu);
+            menu.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == R.id.rename) {
+                    renameRoom(room_id);
+                    return true;
+                }
+                else
+                    return true;
+            });
+            menu.show();
         });
     }
 
@@ -65,5 +91,17 @@ public class ChatActivity extends AppCompatActivity {
             messageListAdapter.addMessage(message);
             messageRecycler.scrollToPosition(messageListAdapter.getItemCount() - 1);
         });
+    }
+
+    public void renameRoom(int room_id) {
+        new MaterialAlertDialogBuilder(this).setView(dialogView)
+                .setPositiveButton("Salva", (dialogInterface, i) -> {
+                    if (TextUtils.isEmpty(textInputLayout.getEditText().getText()))
+                        textInputLayout.setError("Inserisci un nome");
+                    else
+                        ConnectionHandler.getInstance().doRequest(ChatController.getUpdateRoomRequest(room_id, textInputLayout.getEditText().getText().toString()));
+                })
+                .setNegativeButton("Annulla", null)
+                .show();
     }
 }
