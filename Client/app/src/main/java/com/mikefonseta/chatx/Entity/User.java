85055c6ec1dfe6568b8d555cc040d3b39138d7ee
Gae24 <96017547+Gae24@users.noreply.github.com>
2023-05-17 19:00:00 +0200

    non usati

--- Client/app/src/main/java/com/mikefonseta/chatx/Entity/User.java
+++ Client/app/src/main/java/com/mikefonseta/chatx/Entity/User.java
@@ -4,15 +4,8 @@ public class User {
 
     private int user_id;
     private String username;
-    private String password;
 
     public User() {
-    }
-
-    public User(int user_id, String username, String password) {
-        this.user_id = user_id;
-        this.username = username;
-        this.password = password;
     }
 
     public int getUser_id() {
